6380075020a92a2f0bb92d15cb59c65dbd1fb185
Guy Cole <guycole@gmail.com>
2025-01-15 16:02:15 -0800

    prep for systemd test

--- src/django/mellow/skunk/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Heeler, Hyena, Host, Task
-
-admin.site.register(Heeler)
-admin.site.register(Host)
-admin.site.register(Hyena)
-admin.site.register(Task)
